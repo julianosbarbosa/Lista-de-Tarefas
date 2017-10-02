6d1671fa37931fba807543bd277a546c51e3ed8d
Juliano <juliano@nationphone.com>
2017-10-02 10:02:06 -0300

    UPDATE - mudança de estado

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective} from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
 import { EditarTarefaComponent } from './editar';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -16,7 +17,8 @@ import { EditarTarefaComponent } from './editar';
   declarations: [
     ListarTarefaComponent,
     CadastrarTarefaComponent,
-    EditarTarefaComponent
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
   ],
   providers: [
     TarefaService
